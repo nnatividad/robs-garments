503d113b4ae09d2d829074d11731f0eb81b18d7f
nnatividad <natetividad@gmail.com>
2026-01-27 20:57:15 -0800

    Finished landing page - logo in middle is clickable link that will be routed to shop page - footer added at the bottom

--- src/app/pages/LandingPage.js
+++ src/app/pages/LandingPage.js
@@ -1,14 +1,18 @@
 import Image from "next/image";
 import styles from './landingPage.module.css'
 import NavBar from "@/components/NavBar/NavBar.js"
+import Footer from "@/components/Footer/Footer.js"
 
 export default function LandingPage () {
     return (
         <main>
-            <div className={styles.landingGIF}>
-                <img src="/robin-garments.gif" alt="loading..." />
-            </div>
             <NavBar />
+            <a href='#'>
+                <div className={styles.landingGIF}>
+                <img src="/robin-garments.gif" alt="loading..." />
+                </div>
+            </a>
+            <Footer />
         </main>
     );
 }
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -4,15 +4,15 @@ export default function NavBar() {
     return (
         <div className={styles.navbar}>
             <ul>
-                <a href='#'><li>SEARCH</li></a>
+                <a href='#'><li>SHOP</li></a>
                 <a href='#'><li>CART</li></a>
             </ul>
             <div className={styles.logo}>
-                <img src="/robin-garment-logo.jpg" alt="navbar-logo" width="200px"/>
+                <img src="/robin-garment-logo.jpg" alt="navbar-logo" width="150px"/>
             </div>
             <ul>
-                <a href='#'><li>SHOP</li></a>
                 <a href='#'><li>ARCHIVE</li></a>
+                <a href='#'><li>ABOUT</li></a>
             </ul>
         </div>
     );
