664c7d91482bbcc7161e954cba0a0124ce950973
nnatividad <natetividad@gmail.com>
2026-02-04 14:55:42 -0800

    - Added sanity client - GROQ query to pull item data from Sanity to frontend - Display item on  landing page

--- src/app/pages/LandingPage.js
+++ src/app/pages/LandingPage.js
@@ -2,17 +2,54 @@ import Image from "next/image";
 import styles from './landingPage.module.css'
 import NavBar from "@/components/NavBar/NavBar.js"
 import Footer from "@/components/Footer/Footer.js"
+import { client } from '@/sanity/client'
 
-export default function LandingPage () {
+const ITEMS_QUERY = `*[_type=="item"]{
+  _id,
+  category,
+  price,
+  name,
+  "imageUrls": images[].asset->url,
+  slug
+}`;
+
+export default async function LandingPage () {
+    const items = await client.fetch(ITEMS_QUERY, {});
     return (
         <main>
-            <NavBar />
+            <header>
+                <NavBar />
+            </header>
             <a href='#'>
                 <div className={styles.landingGIF}>
                 <img src="/robin-garments.gif" alt="loading..." />
                 </div>
             </a>
-            <Footer />
+            <section>
+                <h1>New Arrivals</h1>
+                <ul>
+                    {items.map((item) => (
+                        <li key={item._id}>
+                            <a href={`/items/${item.slug?.current}`}>
+                            {item.imageUrls?.[0] && (
+                                <Image
+                                    src={item.imageUrls[0]}
+                                    alt={item.name}
+                                    width={300}
+                                    height={300}
+                                />
+                            )}
+                            </a>
+                            <h3>{item.name}</h3>
+                            <h3>${item.price.toFixed(2)} USD</h3>
+                            <p>{item.category}</p>
+                        </li>
+                    ))}
+                </ul>
+            </section>
+            <footer>
+                <Footer />
+            </footer>
         </main>
     );
 }
