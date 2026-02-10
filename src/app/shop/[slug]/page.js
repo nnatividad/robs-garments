652ec3ef26bf0de3dda6f5dabdc7dac00a9e57ac
nnatividad <natetividad@gmail.com>
2026-02-09 17:30:57 -0800

    - Displaying all item details on page - Updated global styles

--- src/app/shop/[slug]/page.js
+++ src/app/shop/[slug]/page.js
@@ -1,13 +1,59 @@
 import { client } from '@/sanity/client'
+import NavBar from '@/components/NavBar/NavBar'
+import Footer from '@/components/Footer/Footer'
+import Image from 'next/image'
 
 export default async function ItemDetails({ params }){
     const { slug } = await(params);
-    const ITEM_QUERY = `*[_type=="item" && slug.current== $slug][0]`;
+    const ITEM_QUERY = `*[_type=="item" && slug.current== $slug][0]{..., "imageUrls": images[].asset->url}`;
     const item = await client.fetch(ITEM_QUERY, { slug });
-
     return(
         <main>
-            <h1>{item.name}</h1>
+            <header>
+                <NavBar/>
+            </header>
+            <section>
+                <div>
+                    <ul>
+                        {item.imageUrls.map((src,index) =>(
+                                <li key={index}>
+                                    <Image 
+                                        src={src}
+                                        alt={`${item.name} ${index + 1}`}
+                                        width={300}
+                                        height={300}
+                                    />
+                                </li>
+                            ))
+                        }
+                    </ul>
+                </div>
+                <div>
+                    <h1>{item.name}</h1>
+                    <h3>{item.price}</h3>
+                    <button>Add to Cart</button>
+                    <button>Purchase</button>
+                </div>
+                <div>
+                    <h3>Item Details</h3>
+                    <ul>
+                        <li>{item.condition}</li>
+                        <li>{item.color}</li>
+                        {item?.description && <li>{item.description}</li>}
+                    </ul>
+                    <h3>Measurements</h3>
+                    <ul>
+                        <li>{item.size}</li>
+                        {item?.chest && <li>{item.chest} in.</li>}
+                        {item?.length && <li>{item.length} in.</li>}
+                        {item?.waist && <li>{item.waist} in.</li>}
+                        {item?.inseam && <li>{item.inseam} in.</li>}
+                    </ul>
+                </div>
+            </section>
+            <footer>
+                <Footer/>
+            </footer>
         </main>
     )
 }
