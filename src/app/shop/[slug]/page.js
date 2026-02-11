502346b3c6548a1d858d5a2154426f3fb876cadd
nnatividad <natetividad@gmail.com>
2026-02-10 16:23:44 -0800

    - Turned itemInfo into a sticky element so that remains in view as user scrolls through item images.

--- src/app/shop/[slug]/page.js
+++ src/app/shop/[slug]/page.js
@@ -15,7 +15,7 @@ export default async function ItemDetails({ params }){
             </header>
             <section>
                 <div className={styles.basicInfo}>
-                    <div>
+                    <div className={styles.images}>
                         <ul>
                             {item.imageUrls.map((src,index) =>(
                                     <li key={index}>
@@ -31,24 +31,26 @@ export default async function ItemDetails({ params }){
                             }
                         </ul>
                     </div>
-                    <div className={styles.itemInfo}>
-                        <h1>{item.name}</h1>
-                        <h3>${item.price.toFixed(2)} USD</h3>
-                        <button type="button" className={styles.button}>Add to Cart</button>
-                        <button type="button" className={styles.button}>Purchase</button>
+                    <div className={styles.stickInfo}>
+                        <div className={styles.itemInfo}>
+                            <h1>{item.name}</h1>
+                            <h3>${item.price.toFixed(2)} USD</h3>
+                            <button type="button" className={styles.button}>Add to Cart</button>
+                            <button type="button" className={styles.button}>Purchase</button>
 
-                        <div className={styles.details}>
-                            Details:
-                            <ul>
-                                <li>Condition: {item.condition}</li>
-                                <li>Color: {item.color}</li>
-                                {item?.description && <li>{item.description}</li>}
-                                <li>{item.size}</li>
-                                {item?.chest && <li>Chest: {item.chest} in.</li>}
-                                {item?.length && <li>Length: {item.length} in.</li>}
-                                {item?.waist && <li>Waist: {item.waist}.</li>}
-                                {item?.inseam && <li>Inseam: {item.inseam} in.</li>}
-                            </ul>
+                            <div className={styles.details}>
+                                Details:
+                                <ul>
+                                    <li>Condition: {item.condition}</li>
+                                    <li>Color: {item.color}</li>
+                                    {item?.description && <li>{item.description}</li>}
+                                    <li>{item.size}</li>
+                                    {item?.chest && <li>Chest: {item.chest} in.</li>}
+                                    {item?.length && <li>Length: {item.length} in.</li>}
+                                    {item?.waist && <li>Waist: {item.waist}.</li>}
+                                    {item?.inseam && <li>Inseam: {item.inseam} in.</li>}
+                                </ul>
+                            </div>
                         </div>
                     </div>
                 </div>
