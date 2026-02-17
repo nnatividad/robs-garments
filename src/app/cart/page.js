3916326d5093ef920b9919162cc8a81e2c655d47
nnatividad <natetividad@gmail.com>
2026-02-17 13:01:43 -0800

    Displaying Cart Item Data - saved local cart state for cart page that stores item data - seperate cart context from local cart - useEffect() hook for fetching cartItems asynchrounosly within the Cart client component

--- src/app/cart/page.js
+++ src/app/cart/page.js
@@ -2,14 +2,35 @@
 
 import NavBar from '@/components/NavBar/NavBar'
 import Footer from '@/components/Footer/Footer'
+import Image from 'next/image'
 import { useCart } from '../../app/context/CartContext'
+import { client } from '@/sanity/client'
+import { useState, useEffect } from 'react'
 
 // cart stores itemIDs
 // fetch itemDetails: name, image, price using GROQ Query
-
+const CART_QUERY = `*[_type=="item" && _id in $ids]{_id, name, price, "imageUrls": images[].asset->url}`;
 
 export default function Cart(){
-    const cartData = useCart();
+    const cartData = useCart(); // global cart context
+    const [localCart, setLocalCart] = useState([]); // local cart storing item data
+    
+    useEffect(() => {
+
+        if (cartData.cart.length == 0){
+            setLocalCart([]);
+            return;
+        }
+        
+        // query cart items by id whenever the cart array is updated
+        async function fetchItems(){
+            const cartItems = await client.fetch(CART_QUERY, {ids: cartData.cart});
+            setLocalCart(cartItems);
+        }
+
+        fetchItems()
+    }, [cartData.cart]);
+
     return(
         <main>
             <header>
@@ -18,13 +39,23 @@ export default function Cart(){
             <section>
                 <div>
                     <ul>
-                        {cartData.cart.length > 0 ? cartData.cart.map((item) => (
-                            <li key={item}>{item}</li>
+                        {localCart.length  > 0 ? localCart.map((item) => (
+                            <li key={item._id}>
+                                <div>
+                                    <p>{item.name}</p>
+                                    <p>{item.price}</p>
+                                    <Image
+                                    src={item.imageUrls[0]}
+                                    alt={`${item.name}`}
+                                    width = {300}
+                                    height = {300}
+                                    />
+                                </div>
+                            </li>
                         )) :
                             `No Items in Cart`
                         }
                     </ul>
-
                 </div>
             </section>
             <footer>
