ffffa08d460aecffd881b705e811949c3f2201b9
nnatividad <natetividad@gmail.com>
2026-02-24 15:18:20 -0800

    Checkout route handler receives and fetches product data from cart

--- src/app/api/checkout/route.js
+++ src/app/api/checkout/route.js
@@ -1,8 +1,12 @@
 import { NextResponse } from 'next/server'
+import { client } from '../../../../sanity/client'
+
+const ITEMS_QUERY = `*[_type=="item" && _id in $ids]{_id, name, price, "imageUrls": images[].asset->url, isSold}`;
 
 export async function POST(req){
-    const body = await req.json();
-    const cart = body;
-    console.log(body);
+    const body = await req.json(); // receives array of productIDs
+    const cart = await client.fetch(ITEMS_QUERY, {ids:body}); // queries product data: name, price, images
+    console.log(cart);
+    
     return NextResponse.json(cart);
 }
