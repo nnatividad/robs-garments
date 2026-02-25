9d1f9c012b319a63bd7aeb2549d869ea423a77e0
nnatividad <natetividad@gmail.com>
2026-02-24 21:43:22 -0800

    Checking items in cart for isSold

--- src/app/api/checkout/route.js
+++ src/app/api/checkout/route.js
@@ -6,6 +6,14 @@ const ITEMS_QUERY = `*[_type=="item" && _id in $ids]{_id, name, price, "imageUrl
 export async function POST(req){
     const body = await req.json(); // receives array of productIDs
     const cart = await client.fetch(ITEMS_QUERY, {ids:body}); // queries product data: name, price, images
+
+    // check if items are sold or not
+    for(let i = 0; i < cart.length; i++){
+        if (cart[i].isSold){
+            const soldItemName = cart[i].name;
+            return NextResponse.json({ error: `${soldItemName} already sold!`});
+        }
+    }
     console.log(cart);
     
     return NextResponse.json(cart);
